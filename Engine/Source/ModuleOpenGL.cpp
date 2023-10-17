c43e355b19789da9b6c1f23df2fe79d8f2bd1e6b
M1R4B3L <amirabelmartinez@gmail.com>
2023-10-17 19:58:04 +0200

    Opengl Windows

--- Engine/Source/ModuleOpenGL.cpp
+++ Engine/Source/ModuleOpenGL.cpp
@@ -3,6 +3,7 @@
 #include "ModuleOpenGL.h"
 #include "ModuleWindow.h"
 #include "SDL.h"
+#include "GL/glew.h"
 
 ModuleOpenGL::ModuleOpenGL()
 {
@@ -18,11 +19,45 @@ bool ModuleOpenGL::Init()
 {
 	LOG("Creating Renderer context");
 
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+	context = SDL_GL_CreateContext(App->GetWindow()->window);
+
+	GLenum err = glewInit();
+
+	LOG("Using Glew %s", glewGetString(GLEW_VERSION));
+	LOG("Vendor: %s", glGetString(GL_VENDOR));
+	LOG("Renderer: %s", glGetString(GL_RENDERER));
+	LOG("OpenGL version supported %s", glGetString(GL_VERSION));
+	LOG("GLSL: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
 	return true;
 }
 
 update_status ModuleOpenGL::PreUpdate()
 {
+
+	glEnable(GL_DEPTH_TEST); // Enable depth test
+	glEnable(GL_CULL_FACE); // Enable cull backward faces
+	glFrontFace(GL_CCW); // Front faces will be counter clockwise
+
+	int w = 0;
+	int h = 0;
+
+	SDL_GetWindowSize(App->GetWindow()->window, &w, &h);
+
+	glViewport(0, 0, w, h);
+
+	glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 	return UPDATE_CONTINUE;
 }
 
@@ -35,6 +70,9 @@ update_status ModuleOpenGL::Update()
 
 update_status ModuleOpenGL::PostUpdate()
 {
+
+	SDL_GL_SwapWindow(App->GetWindow()->window);
+
 	return UPDATE_CONTINUE;
 }
 
@@ -44,6 +82,7 @@ bool ModuleOpenGL::CleanUp()
 	LOG("Destroying renderer");
 
 	//Destroy window
+	SDL_GL_DeleteContext(context);
 
 	return true;
 }
